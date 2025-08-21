d276b9560e66c0d6fa3aff8059775ff1600b61e1
KyleDDJ <kdejesus16692@student.dmmmsu.edu.ph>
2025-08-21 16:36:04 +0800

    Auth Screens

---
+++ lib/appwrite.ts
@@ -0,0 +1,8 @@
+
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    Platform: "com.order.burgerordering",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: '68a6c7980008bea3b0c4',
+    userCollectionId: '68a6c7d9000725a9b148'
+}
